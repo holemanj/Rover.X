c216e6ce291b2da0a187325ba93b4d437c3e3405
holemanj <John.Holeman@gmail.com>
2014-08-13 15:15:21 -0700

    Adding hardware level init control
    part of project to enhance modularity of code by separating hardware
    specific functions from logic and control

---
+++ header/pic18_init.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   pic18_init.h
+ * Author: holemanj
+ *
+ * Created on August 13, 2014, 3:12 PM
+ * This holds header level info for hardware-level initialization routines
+ */
+
+#ifndef PIC18_INIT_H
+#define	PIC18_INIT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PIC18_INIT_H */
+
---
+++ src/pic18_init.c
@@ -0,0 +1,8 @@
+/*
+ * File:   pic18_init.c
+ * Author: holemanj
+ *
+ * Created on August 13, 2014, 3:12 PM
+ * This holds code for hardware-level (PIC18F45k20) initialization routines
+ */
+
