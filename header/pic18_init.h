cd6f2348c26385e4db7e66a4839cd54d9652eb88
holemanj <John.Holeman@gmail.com>
2014-08-14 12:22:14 -0700

    new file update

--- header/pic18_init.h
+++ header/pic18_init.h
@@ -13,6 +13,84 @@
 extern "C" {
 #endif
 
+/* Configuration bit setup goes here - they are implemented by including this */
+/* header, even if no other pic18_init code is used */
+
+#include <xc.h>
+
+void initPic18Hardware();
+
+/******************************************************************************/
+/* Configuration Bits                                                         */
+/*                                                                            */
+/* For additional information about what the hardware configurations mean in  */
+/* terms of device operation, refer to the device datasheet.                  */
+/*                                                                            */
+/* General C18/XC8 syntax for configuration macros:                           */
+/* #pragma config <Macro Name>=<Setting>, <Macro Name>=<Setting>, ...         */
+/*                                                                            */
+/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
+/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
+/* Configuration Bits, a user controllable configuration bits window is       */
+/* available to Generate Configuration Bits source code which the user can    */
+/* paste into this project.                                                   */
+/*                                                                            */
+/******************************************************************************/
+
+#pragma config FOSC = INTIO67   // Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 18        // Brown Out Reset Voltage bits (VBOR set to 1.8 V nominal)
+
+// CONFIG2H
+#pragma config WDTEN = OFF      // Watchdog Timer Enable bit (WDT is controlled by SWDTEN bit of the WDTCON register)
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config HFOFST = OFF     // HFINTOSC Fast Start-up (The system clock is held off until the HFINTOSC is stable.)
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RE3 input pin enabled; MCLR disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection Block 0 (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection Block 1 (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection Block 2 (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection Block 3 (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection Block 0 (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection Block 1 (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection Block 2 (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection Block 3 (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection Block 0 (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection Block 1 (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection Block 2 (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection Block 3 (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0007FFh) not protected from table reads executed in other blocks)
 
 
 
--- src/pic18_init.c
+++ src/pic18_init.c
@@ -6,3 +6,69 @@
  * This holds code for hardware-level (PIC18F45k20) initialization routines
  */
 
+#include "pic18_init.h"
+
+/* Define system operating frequency */
+#define SYS_FREQ        16000000L
+#define _XTAL_FREQ      16000000L
+
+/* Microcontroller MIPs (FCY) */
+#define FCY             SYS_FREQ/4
+
+
+void initPic18Hardware()
+{
+  /* First configure oscillator; make sure this configuration is */
+  /* consistent with the macros defined above */
+  OSCCON = 0b01110000;    //16MHz on main clock - 4MHz instruction clock
+
+  /* TODO Initialize User Ports/Peripherals/Project here */
+
+  /* Setup analog functionality and port direction */
+
+  //Port A not used for now - set as digital inputs
+  TRISA = 0xFF;
+
+  //Port B is all digital inputs - motor speed pulses
+  TRISB = 0xFF;
+
+  //Port C RC1 and RC2 are PWM outputs. Intialize at outputs - they will be changed later
+  //      RC0 and RC5 are direction outputs
+  //      Rest are currently unused - set as inputs
+  TRISC = 0b11011110;
+
+  //Port D not used for now - set as digital inputs
+
+  TRISD = 0x00;
+  //Port E not used for now - set as digital inputs
+  TRISE = 0x00;
+
+  /* Initialize peripherals */
+  //CCP1 and CCP2 set upfor PWM
+
+  PR2 = 0xFF;     //For 10 bit resolution on the PWM - maybe change later for higher frequency
+
+  CCP1CON = 0b00111100;   //Note - LSb for duty cycle is 11 for 1st 100% experiment
+  CCP2CON = 0b00111100;   //Note - LSb for duty cycle is 11 for 1st 100% experiment
+                          //After experiments, initialize to zero
+
+  CCPR1L = 0x00;          //Initialize to zero speed
+  CCPR2L = 0xA0;
+
+  PIR1bits.TMR2IF = 0;
+  T2CONbits.TMR2ON = 1;
+
+
+
+  while(PIR1bits.TMR2IF==0);  //Wait for first PWM cycle
+
+  TRISC = 0b11011000; //Enable outputs
+
+  PIR1bits.TMR1IF = 0;
+
+  /* Configure the IPEN bit (1=on) in RCON to turn on/off int priorities */
+  RCONbits.IPEN = 1;
+
+  /* Enable interrupts */
+
+}
